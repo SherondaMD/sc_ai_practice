2ad8bd34d9d0925e549192cfb32ae1e0a1e8aaef
SherondaMD <sheronda.mckee@gmail.com>
2023-12-06 21:22:22 -0800

    confirmed axios is working and api is working

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function showAnswer(response) {
 let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
 let context = "be polite and prove a very short answer.";
 let prompt = "when was the first AI created?";
-let apiUrl =
-  "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
