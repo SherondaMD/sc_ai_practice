56b03ad00c313d9a100f1db85a832d6edb797675
SherondaMD <sheronda.mckee@gmail.com>
2023-12-08 08:47:10 -0800

    testing changing h1 via js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function handleClick() {
   alert("hooray");
+  let heading = document.querySelector("h1");
+  heading.style.color = "green";
 }
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
-buttonElement.setAttribute("id", "goodbye");
