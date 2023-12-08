10556c4a3c6411e57a7ab551a436c8662bc5e582
SherondaMD <sheronda.mckee@gmail.com>
2023-12-08 08:30:14 -0800

    adding css for button styling review

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-function showAnswer(response) {
-  alert(response.data.answer);
+function handleClick() {
+  alert("hooray");
 }
 
-let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
-let context = "be polite and provide a very short answer.";
-let prompt = "who was the first female president?";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-console.log("Processing...");
-axios.get(apiUrl).then(showAnswer);
-
+let buttonElement = document.querySelector("#special-button");
+buttonElement.addEventListener("click", handleClick);
