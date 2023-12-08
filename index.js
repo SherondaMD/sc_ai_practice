3cdf7a592d437483735adbb0639730a1ffa5eeb9
SherondaMD <sheronda.mckee@gmail.com>
2023-12-07 21:37:12 -0800

    console logged ai question

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function showAnswer(response) {
-  console.log(response);
+  alert(response.data.answer);
 }
 
 let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
-let context = "be polite and prove a very short answer.";
-let prompt = "when was the first AI created?";
+let context = "be polite and provide a very short answer.";
+let prompt = "who was the first female president?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+console.log("Processing...");
 axios.get(apiUrl).then(showAnswer);
+
