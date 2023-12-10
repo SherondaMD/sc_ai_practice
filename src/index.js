6b2ab52c6b8912c1e012b2a0ed5bc2cb08e08dcf
SherondaMD <sheronda.mckee@gmail.com>
2023-12-09 19:52:41 -0800

    added sc api and corrected order of operations

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
-let context = "Please be clear and polite.";
-let prompt = "Please tell me a joke that is family-friendly.";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-console.log("Let me think...one moment please...");
-
-let buttonElement = document.querySelector("#joke");
-buttonElement.addEventListener("click", showAnswer);
-
 function showAnswer(response) {
   new Typewriter("#joke", {
     strings: response.data.answer,
     autoStart: true,
-    cursor: "",
+    cursor: null,
     delay: 15,
   });
 }
+
+function jokeGenerator(event) {
+  event.preventDefault();
+  let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
+  let context = "Please be clear and polite.";
+  let prompt = "Please tell me a joke that is family-friendly.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeGeneratorElement = document.querySelector("#joke");
+  jokeGeneratorElement.innerHTML =
+    "Thinking of a family-friendly joke, one moment please...";
+
+  axios.get(apiUrl).then(showAnswer);
+}
+let buttonElement = document.querySelector("#question");
+buttonElement.addEventListener("click", jokeGenerator);
