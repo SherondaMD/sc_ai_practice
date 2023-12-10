0f66818ba28d4cd90b95d6a0c1dcd9fc69fdbe3d
SherondaMD <sheronda.mckee@gmail.com>
2023-12-09 19:37:10 -0800

    setting up joke generator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-let challengeTwo = document.querySelector("#challenge-2");
+let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
+let context = "Please be clear and polite.";
+let prompt = "Please tell me a joke that is family-friendly.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-challengeTwo.innerHTML = "Sheronda McKee-Dollar";
+console.log("Let me think...one moment please...");
 
-new Typewriter("#challenge-3", {
-  strings: "Sheronda McKee-Dollar",
-  autoStart: true,
-  cursor: "",
-});
+let buttonElement = document.querySelector("#joke");
+buttonElement.addEventListener("click", showAnswer);
+
+function showAnswer(response) {
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 15,
+  });
+}
