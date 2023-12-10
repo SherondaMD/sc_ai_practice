3d5f959a1c21ff2eaec475a826b7e6b1a48de09c
SherondaMD <sheronda.mckee@gmail.com>
2023-12-09 18:39:47 -0800

    typewrite js my full name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 let challengeTwo = document.querySelector("#challenge-2");
 
 challengeTwo.innerHTML = "Sheronda McKee-Dollar";
+
+new Typewriter("#challenge-3", {
+  strings: "Sheronda McKee-Dollar",
+  autoStart: true,
+  cursor: "",
+});
