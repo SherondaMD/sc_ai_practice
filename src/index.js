42647b5894c070efe381dc67eb9fead841e6d612
SherondaMD <sheronda.mckee@gmail.com>
2023-12-09 17:07:01 -0800

    working on class names to get challenges working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 function handleClick() {
-  let heading = document.querySelector("h1");
-  heading.classList.add("hidden");
+  alert("hooray");
 }
 
-let buttonElement = document.querySelector("#special-button");
+let buttonElement = document.querySelector("#challenge-1");
 buttonElement.addEventListener("click", handleClick);
+buttonElement.setAttribute("disabled", "disabled");
+
+function changeColor() {
+  let boxElement = document.querySelector("challenge-2");
+  boxELement.style.color = "green";
+}
+
+let boxElement = document.querySelector("#challenge-2");
+boxElement.addEventListener("click", "changeColor");
